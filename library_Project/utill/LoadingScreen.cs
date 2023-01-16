5b66593575602b155a14f2eae2627f5bcba4da41
Hyun <hickee32@gmail.com>
2023-01-16 09:40:29 +0900

    utill
    0.5

---
+++ library_Project/utill/LoadingScreen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadingWindow {
+
+    public enum TypeOfMessage {
+        Success,
+        Warning,
+        Error,
+    }
+
+    public static class LoadingScreen {
+        
+        static LoadingWindows sf = null;
+        
+        //lOADING SCREEN 없다면 띄우기
+        public static void ShowSplashScreen() {
+            if (sf==null) {
+                sf = new LoadingWindows();
+                sf.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+                sf.ShowSplashScreen();
+            }
+        }
+        //lOADING SCREEN 있다면 지우기
+        public static void CloseSplashScreen() {
+            if (sf!=null) {
+                sf.CloseSplashScreen();
+                sf = null;
+            }
+        }
+    }
+}
